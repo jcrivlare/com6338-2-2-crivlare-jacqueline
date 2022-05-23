44136912fa11c4dba36f5cef8b462eb910a8639b
Jackie Crivlare <jcrivlare@ufl.edu>
2022-05-22 17:26:03 -0700

    final

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 function greet() {
-    let name = prompt ("What is your name?");
+    var name = prompt ("What is your name?");
     alert("Hello, " + name);
-    let age = prompt ("How old are you?");
-    const myNumber = '3';
-    console.log(2 + parseInt(myNumber));
-    // returns 5
-    VarDate = confirm("Have you had a birthday yet this year?");
-    confirm("Have you had a birthday yet this year?")
-    true
-    confirm("Have you had a birthday yet this year?")
-    false
-    const answer = confirm("Have you had a birthday yet this year?")
-    undefined
+    var age = prompt ("How old are you?");
+    var ageNum = parseInt(age);
+    var answer = confirm("Have you had a birthday yet this year?")
+
+    var currentYear = new Date().getFullYear();
+
     if (answer) {
-        console.log('User wants to proceed')
+        var yearBorn = currentYear - ageNum;
+    } else {
+        var yearBorn = currentYear - ageNum - 1;
     }
-    function getBirthYear(age){
-        var currentDate = new Date();
-        var currentYear = currentDate.getFullYear();
-        birthYear = currentYear - age;
-        return birthYear;
-    }
-    calculatedBirthYear = getBirthYear(age); {
-        const currentYear = new Date().getFullYear();
-        return currentYear - age;
-    } 
-    if (answer=false) {}
-    else {
-        return 'currentYear - age + 1';
-    }
+
+    alert("You were born in " + yearBorn);
     }
